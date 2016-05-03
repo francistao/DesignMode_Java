b8bfa4190db682b0bd8a7892b10e697d09ceaeb2
geniusvjr <geniusvjr@gmail.com>
2016-05-03 11:19:53 +0800

    Adapter

---
+++ Adapter/src/DefaultAdapter.java
@@ -0,0 +1,26 @@
+
+import java.util.List;
+
+public class DefaultAdapter implements LogDbOperateApi{
+
+	@Override
+	public void createLog(LogModel lm) {
+		System.out.println("DB createLog===========");
+	}
+
+	@Override
+	public void updateLog(LogModel lm) {
+		System.out.println("DB updateLog===========");
+	}
+
+	@Override
+	public void removeLog(LogModel lm) {
+		
+	}
+
+	@Override
+	public List<LogModel> getAllLog() {
+		return null;
+	}
+
+}
---
+++ Adapter/src/TimeUtil.java
@@ -0,0 +1,15 @@
+
+public class TimeUtil {
+	private long a1;
+	private long a2;
+	
+	public void begin(){
+		a1 = System.currentTimeMillis();
+	}
+	public void end(){
+		a2 = System.currentTimeMillis();
+	}
+	public void show(){
+		System.out.println("times11==="+(a2-a1));
+	}
+}
